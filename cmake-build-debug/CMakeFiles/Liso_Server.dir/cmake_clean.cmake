bb7d602d237386ba117f3a2a66e8e9883ff6308f
Sean Hsu <seanhsu@Seans-MacBook-Air.local>
2017-09-11 17:24:58 -0400

    Initial Starter Pack

---
+++ cmake-build-debug/CMakeFiles/Liso_Server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Liso_Server.dir/echo_server.c.o"
+  "CMakeFiles/Liso_Server.dir/echo_client.c.o"
+  "Liso_Server.pdb"
+  "Liso_Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Liso_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Liso_Server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Liso_Server.
+# This may be replaced when dependencies are built.
